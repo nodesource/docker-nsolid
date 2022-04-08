f3438b083a80c92177cb3bd3d4eb839d049628d4
Jesus Hernando Paz Bolaños <jesus@nodesource.com>
2022-04-08 12:08:55 -0500

    Deprecate Erbium

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-declare -a versions=("erbium" "fermium" "gallium")
+declare -a versions=("fermium" "gallium")
 declare -a images=("nsolid" "nsolid-console" "nsolid-storage" "nsolid-cli")
 
 latest=${NSOLID_LTS_LATEST:-'gallium'}
@@ -8,46 +8,46 @@ registry=${DOCKER_REGISTRY:-'nodesource'}
 release=${NSOLID_VERSION}
 
 if [ "$BUILD_ALPINE" == "1" ]; then
-  declare -a versions=("erbium-alpine" "fermium-alpine" "gallium-alpine")
+    declare -a versions=("fermium-alpine" "gallium-alpine")
 fi
 
 if [[ ${NSOLID_VERSION} =~ ^3\.(.*)\.(.*) ]] || [[ ${NSOLID_VERSION} =~ ^4\.(.*)\.(.*) ]]; then
-  declare -a is4="true"
+    declare -a is4="true"
 else
-  declare -a is4="false"
+    declare -a is4="false"
 fi
 
 for lts in "${versions[@]}"
 do
-  for img in "${images[@]}"
-  do
-    if [[ $is4 == "true" ]] && [[ $img == "nsolid-storage" ]]; then
-      :
-    else
-      docker tag nodesource/$img:$lts $registry/$img:$lts-$release
-      docker push $registry/$img:$lts-$release
-
-      docker tag nodesource/$img:$lts $registry/$img:$lts-latest
-      docker push $registry/$img:$lts-latest
-    fi
-
-    if [ "$lts" == "$latest" ]; then
-      if [[ $is4 == "true" ]] && [[ $img == "nsolid-storage" ]]; then
-        :
-      else
-        docker tag nodesource/$img:$lts $registry/$img:latest
-        docker push $registry/$img:latest
-      fi
-    fi
-
-    if [ "$BUILD_ALPINE" == "1" ]; then
-      if [[ $is4 == "true" ]] && [[ $img == "nsolid-storage" ]]; then
-        :
-      else
-        docker tag nodesource/$img:$lts $registry/$img:alpine
-        docker push $registry/$img:alpine
-      fi
-    fi
-
-  done
+    for img in "${images[@]}"
+    do
+        if [[ $is4 == "true" ]] && [[ $img == "nsolid-storage" ]]; then
+            :
+        else
+            docker tag nodesource/$img:$lts $registry/$img:$lts-$release
+            docker push $registry/$img:$lts-$release
+            
+            docker tag nodesource/$img:$lts $registry/$img:$lts-latest
+            docker push $registry/$img:$lts-latest
+        fi
+        
+        if [ "$lts" == "$latest" ]; then
+            if [[ $is4 == "true" ]] && [[ $img == "nsolid-storage" ]]; then
+                :
+            else
+                docker tag nodesource/$img:$lts $registry/$img:latest
+                docker push $registry/$img:latest
+            fi
+        fi
+        
+        if [ "$BUILD_ALPINE" == "1" ]; then
+            if [[ $is4 == "true" ]] && [[ $img == "nsolid-storage" ]]; then
+                :
+            else
+                docker tag nodesource/$img:$lts $registry/$img:alpine
+                docker push $registry/$img:alpine
+            fi
+        fi
+        
+    done
 done
